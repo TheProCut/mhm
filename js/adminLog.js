de578678a080f9cc15cf6be53db50026b9978837
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-08-13 22:52:25 +0200

    Update adminLog.js

--- js/adminLog.js
+++ js/adminLog.js
@@ -23,19 +23,40 @@ const usernamePrm = urlParams.get('name');
 const useridPrm = urlParams.get('id');
 const cmdPrm = urlParams.get('cmd');
 const authPrm = urlParams.get('auth')
+const rankPrm = urlParams.get('rank')
+const timePrm = urlParams.get('time')
+const placePrm = urlParams.get('place')
+
 const usernameElement = document.getElementById('username');
 const cmdElement = document.getElementById('cmd');
+const rankElement = document.getElementById('rank');
+const timeElement = document.getElementById('time');
+const placeElement = document.getElementById('place');
 const sectionElement = document.getElementById('s1');
 
-function displayInfo(username, userid, command) {
+function displayInfo(username, userid, command, rank, time, place) {
     usernameElement.textContent = username;
     cmdElement.textContent = command;
+    rankElement.textContent = rank;
     
+    // Convert epoch time to human-readable format (assuming timePrm is in seconds)
+    const date = new Date(time * 1000);
+    timeElement.textContent = date.toLocaleString(); // Change format as needed
+    
+    placeElement.textContent = place;
 }
 
 if (!authPrm) {
-  sectionElement.textContent = "You cannot view this page."
+  sectionElement.textContent = "You cannot view this page.";
 } else {
-    displayInfo(usernamePrm, useridPrm, cmdPrm);
+    displayInfo(usernamePrm, useridPrm, cmdPrm, rankPrm, timePrm, placePrm);
 }
 
+function btn(platform) {
+  const playerName = usernamePrm || 'USERNAME'; // Default to 'USERNAME' if not available
+  if (platform === 'Hyra') {
+    window.open(`https://app.hyra.io/6338747abe283e70e7d5b28b/staff/profile/${useridPrm}`, '_blank');
+  } else if (platform === 'Rblx') {
+    window.open(`https://www.roblox.com/users/${useridPrm}/profile`, '_blank');
+  }
+}

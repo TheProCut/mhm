e71c965a56ac3a4dd3c287dafda03a697ca62547
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-08-14 14:07:41 +0200

    Update adminLog.js

--- js/adminLog.js
+++ js/adminLog.js
@@ -18,43 +18,41 @@ _      _
 /* Use: Provides information about the command used, and the user who used it. */
 /* Notes: Script only used on adminlog.html */
 
-const urlParams = new URLSearchParams(window.location.search);
-const usernamePrm = urlParams.get('name');
-const useridPrm = urlParams.get('id');
-const cmdPrm = urlParams.get('cmd');
-const authPrm = urlParams.get('auth')
-const rankPrm = urlParams.get('rank')
-const timePrm = urlParams.get('time')
-const placePrm = urlParams.get('place')
+    const urlParams = new URLSearchParams(window.location.search);
+    const usernamePrm = urlParams.get('name');
+    const useridPrm = urlParams.get('id');
+    const cmdPrm = urlParams.get('cmd');
+    const authPrm = urlParams.get('auth')
+    const rankPrm = urlParams.get('rank')
+    const timePrm = urlParams.get('time')
+    const placePrm = urlParams.get('place')
 
-const usernameElement = document.getElementById('username');
-const cmdElement = document.getElementById('cmd');
-const rankElement = document.getElementById('rank');
-const timeElement = document.getElementById('time');
-const placeElement = document.getElementById('place');
-const sectionElement = document.getElementById('s1');
+    const usernameElement = document.getElementById('username');
+    const cmdElement = document.getElementById('cmd');
+    const rankElement = document.getElementById('rank');
+    const timeElement = document.getElementById('time');
+    const placeElement = document.getElementById('place');
+    const pfpElement = document.getElementById('pfp');
+    const hyrabtn = document.getElementById('hyrabtn');
+    const robloxbtn = document.getElementById('robloxbtn');
 
-const hyrabtn = document.getElementById('hyrabtn');
-const robloxbtn = document.getElementById('robloxbtn');
+    function displayInfo(username, userid, command, rank, time, place) {
+      usernameElement.textContent = username;
+      cmdElement.textContent = command;
+      rankElement.textContent = rank;
+      hyrabtn.textContent = `View ${username}'s Hyra Profile`;
+      robloxbtn.textContent = `View ${username}'s Roblox Profile`;
+      const date = new Date(time * 1000);
+      timeElement.textContent = date.toLocaleString();
+      placeElement.textContent = place;
+      pfpElement.src = `https://bloxtech.tech/admin/api/userthumb.php?userId=${userid}&width=75&height=75&format=png`;
+    }
 
-
-function displayInfo(username, userid, command, rank, time, place) {
-    usernameElement.textContent = username;
-    cmdElement.textContent = command;
-    rankElement.textContent = rank;
-    hyrabtn.textContent = "View "+username+"'s Hyra Profile"
-    robloxbtn.textContent = "View "+username+"'s Roblox Profile"
-    const date = new Date(time * 1000);
-    timeElement.textContent = date.toLocaleString();
-    
-    placeElement.textContent = place;
-}
-
-if (!authPrm) {
-  sectionElement.textContent = "You cannot view this page.";
-} else {
-    displayInfo(usernamePrm, useridPrm, cmdPrm, rankPrm, timePrm, placePrm);
-}
+    if (!authPrm) {
+      sectionElement.textContent = "You cannot view this page.";
+    } else {
+      displayInfo(usernamePrm, useridPrm, cmdPrm, rankPrm, timePrm, placePrm);
+    }
 
 function btn(platform) {
   if (platform === 'Hyra') {

1fb6454ffeabeb75d3ae606db8b54b8d4b7d1af5
bloxcosite <139221902+bloxcosite@users.noreply.github.com>
2023-09-24 12:03:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
       <hr>
       <p>&copy; ${year} Bloxco Supermarket. All rights reserved.</p>
       <span class="disclaimer">This site was made by zertybee_5</span>
-      <p class="fs-6 text-lowercase text-start fw-lighter">.fs-6 text</p>
+      <p class="fs-6 text-lowercase text-start fw-lighter">Site version: 2.01</p>
     </div>
   `;
 
